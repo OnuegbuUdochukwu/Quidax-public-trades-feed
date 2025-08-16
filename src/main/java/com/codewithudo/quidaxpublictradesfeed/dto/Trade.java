b96ae423dc766d51567578b55b0b3f2e515e2ce3
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-08-16 15:00:13 +0100

    Trade dto

---
+++ src/main/java/com/codewithudo/quidaxpublictradesfeed/dto/Trade.java
@@ -0,0 +1,21 @@
+package com.codewithudo.quidaxpublictradesfeed.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class Trade {
+
+    private String type;
+    private String price;
+    private long timestamp;
+
+    @JsonProperty("trade_id")
+    private int tradeId;
+
+    @JsonProperty("base_volume")
+    private String baseVolume;
+
+    @JsonProperty("quote_volume")
+    private String quoteVolume;
+}
