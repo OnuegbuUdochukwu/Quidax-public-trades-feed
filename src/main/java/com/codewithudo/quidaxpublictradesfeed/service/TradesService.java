2aa69aef4b797f97cd1c2bbe04ffa12f2aea7b1d
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-08-16 15:03:16 +0100

    TradeService class

---
+++ src/main/java/com/codewithudo/quidaxpublictradesfeed/service/TradesService.java
@@ -0,0 +1,30 @@
+package com.codewithudo.quidaxpublictradesfeed.service;
+
+import com.codewithudo.quidaxpublictradesfeed.dto.Trade;
+import com.codewithudo.quidaxpublictradesfeed.dto.TradesResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class TradesService {
+
+    private final RestTemplate restTemplate;
+
+    public TradesService() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    public List<Trade> getRecentTrades(String market) {
+        String url = "https://api.quidax.com/api/v1/trades/" + market;
+
+        TradesResponse response = restTemplate.getForObject(url, TradesResponse.class);
+
+        if (response != null && "success".equals(response.getStatus())) {
+            return response.getData();
+        }
+
+        return Collections.emptyList();
+    }
+}
